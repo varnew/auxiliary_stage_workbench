9f46096fc3a9277cf80e89bce34e065b719df9d7
wuxianlin <wuxianlin@yizhicloud.cn>
2021-11-26 11:34:27 +0800

    feat(ALL): edit vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -21,9 +21,9 @@ const assetsCDN = {
   css: [],
   js: [
     "//cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.min.js",
-    "//cdn.jsdelivr.net/npm/vue-router@3.3.4/dist/vue-router.min.js",
+    "//cdn.jsdelivr.net/npm/vue-router@3.5.3/dist/vue-router.min.js",
     "//cdn.jsdelivr.net/npm/vuex@3.4.0/dist/vuex.min.js",
-    "//cdn.jsdelivr.net/npm/axios@0.19.2/dist/axios.min.js",
+    "//cdn.jsdelivr.net/npm/axios@0.24.0/dist/axios.min.js",
     "//cdn.jsdelivr.net/npm/nprogress@0.2.0/nprogress.min.js",
     "//cdn.jsdelivr.net/npm/clipboard@2.0.6/dist/clipboard.min.js",
     "//cdn.jsdelivr.net/npm/@antv/data-set@0.11.4/build/data-set.min.js",
